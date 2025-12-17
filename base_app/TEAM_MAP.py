d4865622975e6bfa415e4b45d2cacfd2235006a5
Aakash Kaushal <159193536+AakashKaushal07@users.noreply.github.com>
2025-12-17 11:43:16 +0530

    Plan game model (#11)
    * change the team alias format and add a lot of team alias
    
    * develop merge logic for event s with both events and shots
    
    * include notebooks to check season merge and add games to the model
    
    ---------
    
    Co-authored-by: Aakash Kaushal <aakashkaushal75@gmail.com>

--- base_app/TEAM_MAP.py
+++ base_app/TEAM_MAP.py
@@ -1,9 +1,907 @@
 TEAM_MAPPING = {
-    "Stade Rennais FC" : "Rennes",
-    "Helsingin Jalkapalloklubi" : "HJK",
-    "CD Santa Clara" : "Santa Clara",
-    "Kuopion Palloseura" : "KuPS",
-    "HNK Rijeka" : "Rijeka",
-    "KS Raków Częstochowa" : "Raków",
-    "Tottenham Hotspur fc": "spurs" 
+  "FK Austria Wien": [
+      "Rapid Wien"
+  ],
+  "Atalanta Bergamasca Calcio": [
+      "Atalanta"
+  ],
+  "FC Zorya Luhansk": [
+    "Zorya"
+  ],
+  "FK Partizan Beograd": [
+    "Partizan Beograd"
+  ],
+  "Anorthosis Famagusta FC": [
+    "Anorthosis",
+    "Famagusta"
+  ],
+  "Alkmaar Zaanstreek": [
+    "AZ Alkmaar",
+    "AZ"
+  ],
+  "AC Omonia Nicosia": [
+    "Omonia Nicosia",
+    "Omonia FC",
+    "Omonia"
+  ],
+  "NŠ Mura": [
+    "Mura"
+  ],
+  "CA Mineiro": [
+    "Atletico MG",
+    "Atletico Mineiro"
+  ],
+  "Royal Charleroi SC": [
+    "Sporting Charleroi"
+  ],
+  "PFK Ludogorets 1945 Razgrad": [
+    "Ludogorets Razgrad"
+  ],
+  "Linzer Athletik Sport Klub": [
+    "LASK"
+  ],
+  "Club Foot Estrela": [
+    "Estrela da Amadora"
+  ],
+  "FK Rīgas Futbola skola": [
+    "RFS"
+  ],
+  "The New Saints FC": [
+    "TNS"
+  ],
+  "FC Midtjylland": [
+    "Midtjylland"
+  ],
+  "Fenerbahçe Spor Kulübü": [
+    "Fenerbahçe"
+  ],
+  "Heart of Midlothian FC": [
+    "Hearts",
+    "Midlothian"
+  ],
+  "SK Sigma Olomouc": [
+    "SK Sigma",
+    "Sigma Olomouc"
+  ],
+  "Burnley FC": [
+    "Burnley"
+  ],
+  "Aberdeen FC": [
+    "Aberdeen"
+  ],
+  "FC Lugano": [
+    "Lugano"
+  ],
+  "HŠK Zrinjski Mostar": [
+    "Zrinjski",
+    "Zrinjski Mostar",
+    "HŠK Zrinjski",
+    "Mostar Zrinjski"
+  ],
+  "GNK Dinamo Zagreb": [
+    "Dinamo Zagreb",
+    "Zagreb"
+  ],
+  "FK Čukarički": [
+    "Čukarički Beograd",
+    "Čukarički"
+  ],
+  "Os Belenenses SAD": [
+    "Belenenses SAD",
+    "Cova da Piedade SAD"
+  ],
+  "BSC Young Boys": [
+    "Young Boys"
+  ],
+  "Maccabi Tel Aviv FC": [
+    "Maccabi Tel Aviv"
+  ],
+  "HNK Hajduk Split": [
+    "Hajduk Split",
+    "Hajduk",
+    "HNK Hajduk"
+  ],
+  "Royal Antwerp FC": [
+    "Antwerp"
+  ],
+  "Ferencvárosi TC": [
+    "Ferencváros"
+  ],
+  "Tic Tapa UE Santa Coloma": [
+    "UE Santa Coloma",
+    "Santa Coloma"
+  ],
+  "FK Bodø / Glimt": [
+    "Bodø/Glimt",
+    "Bodø Glimt",
+    "FK Bodø/Glimt"
+  ],
+
+  "Qarabağ Ağdam FK": [
+    "Qarabağ FK",
+    "Qarabağ Ağdam",
+    "Qarabağ"
+  ],
+  "SK Slavia Praha": [
+    "Slavia Praha",
+    "Slavia Prague"
+  ],
+  "Galatasaray Spor Kulübü": [
+    "Galatasaray",
+    "Galatasaray SK"
+  ],
+  "FC Dynamo Kyiv": [
+    "Dynamo Kyiv",
+    "Dinamo Kyiv"
+  ],
+  "Malmö FF": [
+    "Malmö"
+  ],
+  "FC Viktoria Plzeň": [
+    "Viktoria Plzeň"
+  ],
+  "NK Lokomotiva Zagreb": [
+    "Lokomotiva Zagreb"
+  ],
+  "SK Rapid": [
+    "Rapid Wien",
+    "Rapid Vienna",
+    "Rapid Wienna"
+  ],
+  "Celtic FC": [
+    "Celtic",
+    "Glasgow Celtic"
+  ],
+  "PAOK Thessaloniki FC": [
+   "PAOK",
+   "PAOK FC"
+  ],
+  "SL Benfica": [
+    "Sport Lisboa e Benfica",
+    "Benfica"
+  ],
+  "KAA Gent": [
+    "Gent"
+  ],
+  "Molde FK": [
+    "Molde"
+  ],
+  "Valletta FC": [
+    "Valletta"
+  ],
+  "FC Utrecht": [
+    "Utrecht"
+  ],
+  "KKS Lech Poznań": [
+    "Lech Poznań"
+  ],
+  "PSV Eindhoven": [
+    "PSV"
+  ],
+  "KV Oostende": [
+    "Oostende"
+  ],
+  "MFK Ružomberok": [
+    "Ružomberok",
+    "FK Ružomberok",
+    "Ružomberok FK"
+  ],
+  "SC Freiburg": [
+    "Freiburg"
+   ],
+  "NK Domžale": [
+    "Domžale"
+  ],
+  "Panathinaikos FC": [
+    "Panathinaikos"
+  ],
+  "KF Shkendija": [
+    "Shkendija",
+    "Shkendija Tetovo"
+  ],
+  "PFK CSKA Moskva": [
+    "CSKA Moskva",
+    "CSKA Moscow",
+    "CSKA"
+  ],
+  "FC Basel 1893": [
+    "Basel"
+  ],
+  "İstanbul Başakşehir Futbol Kulübü": [
+    "İstanbul Başakşehir",
+    "Başakşehir"
+  ],
+  "APOEL FC": [
+    "APOEL Nicosia"
+  ],
+  "SCS CFR 1907 Cluj": [
+    "CFR Cluj",
+    "CFR 1907 Cluj"
+  ],
+  "Rosenborg BK": [
+    "Rosenborg"
+  ],
+  "FK Krasnodar": [
+    "Krasnodar"
+  ],
+  "Hyderabad FC": [
+    "Hyderabad",
+    "SC Delhi"
+  ],
+  "FC Zürich": [
+    "Zürich"
+  ],
+  "AEK Larnaca": [
+    "AEK Larnaca",
+    "AEK Larnaka"
+  ],
+  "AS Monaco FC": [
+    "Monaco",
+    "AS Monaco"
+  ],
+  "SK Sturm Graz": [
+    "Sturm Graz"
+  ],
+  "Trabzonspor Kulübü": [
+    "Trabzonspor"
+  ],
+  "FK Crvena zvezda Beograd": [
+    "Crvena zvezda",
+    "Red Star Belgrade",
+    "Red Star",
+    "FK Crvena zvezda",
+    "Crvena zvezda Beograd"
+  ],
+  "Paksi FC": [
+    "Paksi"
+  ],
+  "SC Corvinul 1921 Hunedoara": [
+    "Corvinul Hunedoara"
+  ],
+  "Wisła Kraków": [
+    "Biała Gwiazda",
+    "Wisła Kraków"
+  ],
+  "KF Llapi Podujevë": [
+    "KF Llapi Podujevë",
+    "KF Llapi Podujevo"
+  ],
+  "Silkeborg IF": [
+    "Silkeborg IF"
+  ],
+  "FK Vojvodina Novi Sad": [
+    "Vojvodina"
+  ],
+  "FK Sheriff Tiraspol": [
+    "Sheriff Tiraspol",
+    "FK Sheriff",
+    "Sheriff",
+    "Sheriff FC"
+  ],
+  "Servette FC": [
+    "Servette",
+    "Servette Genève"
+  ],
+  "NSÍ Runavík": [
+    "Runavík"
+  ],
+  "KF Teuta Durrës": [
+    "Teuta Durrës"
+  ],
+  "PFK Lokomotiv Plovdiv 1926": [
+    "Lokomotiv Plovdiv"
+  ],
+  "KS Kukësi": [
+    "KS Kukësi"
+  ],
+  "Bala Town FC": [
+    "Bala Town"
+  ],
+  "Shamrock Rovers FC": [
+    "Shamrock Rovers"
+  ],
+  "Viking FK": [
+    "Viking Stavanger",
+    "Viking"
+  ],
+  "Stade de Reims": [
+    "Reims"
+  ],
+  "Fehérvár FC": [
+    "Fehérvár FC",
+    "Vidi",
+    "MOL Vidi FC",
+    "MOL Fehérvár FC"
+  ],
+  "Riga FC": [
+    "Riga"
+  ],
+  "FC Lokomotivi Tbilisi": [
+    "Lokomotivi Tbilisi"
+  ],
+  "Sporting Clube de Portugal": [
+    "Sporting",
+    "Sporting Lisbon",
+    "Sporting CP"
+  ],
+  "FK Sarajevo": [
+    "Sarajevo"
+  ],
+  "Rio Ave FC": [
+    "Rio Ave"
+  ],
+  "Maccabi Haifa FC": [
+    "Maccabi Haifa"
+  ],
+  "FC Salzburg": [
+    "Salzburg",
+    "Red Bull Salzburg",
+    "FC RB Salzburg",
+    "RB Salzburg"
+  ],
+  "FK BATE Borisov": [
+    "BATE"
+  ],
+  "OGC Nice Côte d'Azur": [
+    "OGC Nice",
+    "Nice"
+  ],
+  "AFC Ajax": [
+    "Ajax",
+    "Ajax Amsterdam"
+  ],
+  "FC København": [
+    "København",
+    "Copenhagen",
+    "FC Copenhagen"
+  ],
+  "SC Fotbal Club FCSB SA": [
+    "FCSB"
+  ],
+  "TSG 1899 Hoffenheim": [
+    "TSG Hoffenheim",
+    "Hoffenheim"
+  ],
+  "FK Astana": [
+    "Astana"
+  ],
+  "Hapoel Be'er Sheva FC": [
+    "Hapoel Be'er Sheva",
+    "Hapoel Beer Sheva",
+    "Hapoel Beersheba"
+  ],
+  "HNK Rijeka": [
+    "Rijeka",
+    "Rijeka FC"
+  ],
+  "AC Sparta Praha": [
+    "Sparta Praha",
+    "Sparta Prague"
+  ],
+  "ŠK Slovan Bratislava": [
+    "Slovan Bratislava"
+  ],
+  "FK Spartak Moskva": [
+    "Spartak Moskva",
+    "Spartak Moskow",
+    "Spartak Moscow",
+    "Spartak"
+  ],
+  "Brøndby IF": [
+    "Brøndby"
+    ],
+  "Montevideo Wanderers FC": [
+    "Montevideo Wanderers"
+  ],
+  "Club Olimpia": [
+    "Olimpia",
+    "Club Olimpia",
+    "Olimpia Asunción"
+  ],
+  "CSD Macará": [
+    "Macará"
+  ],
+  "CD Oriente Petrolero": [
+    "Oriente Petrolero",
+    "Oriente"
+  ],
+  "Club Universitario de Deportes": [
+    "Universitario",
+    "Universitario de Deportes"
+  ],
+  "Club Guaraní": [
+    "Guaraní"
+  ],
+  "CA Banfield": [
+    "Banfield",
+    "Club Atlético Banfield",
+    "El Taladro"
+  ],
+  "CD Santiago Wanderers": [
+    "Santiago Wanderers"
+  ],
+  "FBC Melgar": [
+    "Meglar"
+  ],
+  "Chapecoense AF": [
+    "Chapecoense",
+    "AFC Chapecoense"
+  ],
+  "Club Jorge Wilstermann": [
+    "Jorge Wilstermann",
+    "Wilstermann",
+    "Wilstermann FC"
+  ],
+  "Carabobo FC": [
+    "Carabobo"
+  ],
+  "CD San José": [
+    "San Jose Oruro"
+  ],
+  "CA Progreso": [
+    "Progreso",
+    "Club Athletico Progreso"
+  ],
+  "Club Universidad de Chile": [
+    "Universidad de Chile"
+  ],
+  "Club Deportes Tolima SA": [
+    "Tolima"
+  ],
+  "Deportivo Táchira FC": [
+    "Táchira",
+    "Deportivo Táchira"
+  ],
+
+  "CD Palestino": [
+    "Palestino"
+  ],
+  "Club Cerro Porteño": [
+    "Cerro"
+  ],
+  "SC Corinthians Paulista": [
+    "Corinthians"
+  ],
+  "Club The Strongest": [
+    "The Strongest"
+  ],
+  "CA Tucumán": [
+    "Atlético Tucumán",
+    "Tucumán"
+  ],
+  "Club Sporting Cristal SA": [
+    "Sporting Cristal"
+  ],
+  "RWD Molenbeek": [
+    "RWDM Brussels",
+    "Molenbeek"
+  ],
+  "Delfín SC": [
+    "Deflin"
+  ],
+  "Club Bolívar": [
+    "Bolivar"
+  ],
+  "Cusco FC": [
+    "Cusco",
+    "Real Garcilaso"
+  ],
+
+  "Caracas FC": [
+    "Caracas"
+  ],
+  "CA Talleres": [
+    "Talleres",
+    "Club Athletico Talleres"
+  ],
+  "São Paulo FC": [
+    "São Paulo FC"
+  ],
+  "Club Sport Huancayo": [
+    "Sport Huancayo"
+  ],
+  "CA Boston River": [
+    "Boston River",
+    "Club Athletico Boston River"
+  ],
+  "Zamora FC": [
+    "Zamora"
+  ],
+  "CD Provincial Curicó Unido": [
+    "Curicó Unido"
+  ],
+  "CA Huracán": [
+    "Huracán"
+  ],
+  "CD Magallanes": [
+    "Magallanes"
+  ],
+  "Club Always Ready": [
+    "Always Ready"
+  ],
+  "CD Maldonado": [
+    "Maldonado"
+  ],
+  "Fortaleza EC": [
+    "Fortaleza"
+  ],
+  "CD Universidad Católica del Ecuador": [
+    "Universidad Católica"
+  ],
+  "Millonarios FC SA": [
+    "Millonarios"
+  ],
+  "FC Struga Trim-Lum": [
+    "Struga"
+  ],
+  "FK Žalgiris Vilnius": [
+    "Žalgiris"
+  ],
+  "Hamrun Spartans FC": [
+    "Hamrun"
+  ],
+  "FK Valmiera": [
+    "Valmiera"
+  ],
+  "NK Olimpija Ljubljana": [
+    "Olimpija Ljubljana"
+  ],
+  "KÍ Klaksvík": [
+    "Klaksvik"
+  ],
+  "FC Swift Hesperange": [
+    "Swift Hesperange"
+  ],
+  "SK Dnipro-1": [
+    "Dnipro-1"
+  ],
+  "KRC Genk": [
+    "Genk"
+  ],
+  "Aris FC Limassol": [
+    "Aris Limassol"
+  ],
+  "KS Raków Częstochowa": [
+    "Rakow Czestochowa",
+    "KS Rakow Czestochowa",
+    "RKS Rakow Czestochowa",
+    "Rakow"
+  ],
+  "Breidablik UBK": [
+    "Breidablik",
+    "Breidablik Kopavogur"
+  ],
+  "Rangers FC": [
+    "Rangers",
+    "Glasgow Rangers"
+  ],
+  "Montevideo City Torque": [
+    "Montevideo Torque"
+  ],
+  "Everton de Viña del Mar": [
+    "Everton Vina del Mar",
+    "Everton CD",
+    "Everton Chile"
+  ],
+  "Monagas SC": [
+    "Monagas"
+  ],
+  "Fluminense FC": [
+    "Fluminense Rio de Janeiro",
+    "Fluminense"
+  ],
+  "Audax Italiano La Florida": [
+    "Audax Italiano",
+    "Audax La Florida"
+  ],
+  "FC Chikhura Sachkhere": [
+    "Chikhura Sachkhere",
+    "Chikhura",
+    "Sachkhere FC"
+  ],
+  "FC Flora Tallinn": [
+    "Flora Tallinn",
+    "Flora"
+  ],
+  "BK Häcken": [
+    "Hacken"
+  ],
+  "RC Strasbourg Alsace": [
+    "RC Strasbourg",
+    "Strasbourg"
+  ],
+  "Crusaders FC": [
+    "Crusaders",
+    "Crusaders Belfast"
+  ],
+  "Torino FC": [
+    "Torino Calcio",
+    "Torino"
+  ],
+  "Debreceni VSC": [
+    "Debreceni",
+    "Debrecen"
+  ],
+  "FK Haugesund": [
+    "Haugesund"
+  ],
+  "Pyunik FC": [
+    "Pyunik Yerevan",
+    "Pyunik"
+  ],
+  "FK Mariupol": [
+    "Mariupol"
+  ],
+  "FC Dinamo Tbilisi": [
+    "Dinamo Tbilisi"
+  ],
+  "FC Vaduz": [
+    "Vaduz Liechtenstein",
+    "Vaduz"
+  ],
+  "Apollon Limassol FC": [
+    "Apollon Limassol"
+  ],
+  "FC Luzern": [
+    "Luzern Switzerland",
+    "Luzern"
+  ],
+  "FK Shakhtyor Soligorsk": [
+    "Shakhtyor Soligorsk",
+    "Shakhtyor"
+  ],
+  "AEK Athens FC": [
+    "AEK Athens",
+    "AEK"
+  ],
+  "Universitatea Craiova 1948 Club Sportiv": [
+    "Universitatea Craiova"
+    ],
+  "Liverpool FC Montevideo": [
+    "Liverpool Montevideo",
+    "Liverpool Uruguay",
+    "Liverpool de Montevideo"
+    ],
+  "CD Royal Pari": [
+    "CD Royal Pari"
+  ],
+  "AC Lara": [
+    "Lara"
+  ],
+
+  "Club Libertad": [
+    "Libertad Paraguay",
+    "Libertad"
+  ],
+  "CA San Lorenzo de Almagro": [
+    "San Lorenzo de Almagro",
+    "San Lorenzo"
+  ],
+  "Grêmio FB Porto Alegrense": [
+    "Gremio Porto Alegrense",
+    "Gremio"
+  ],
+  "Ayacucho FC": [
+    "Ayacucho FC Peru",
+    "Ayacucho"
+  ],
+  "Academia Puerto Cabello": [
+    "Puerto Cabello",
+    "APC Puerto Cabello"
+  ],
+  "Defensor Sporting Club": [
+    "Defensor Sporting",
+    "Defensor",
+    "Defensor SC"
+  ],
+  "SD Aucas": [
+    "Sociedad Deportiva Aucas",
+    "Aucas"
+  ],
+  "Portuguesa FC": [
+    "Portuguesa Venezuela",
+    "Portuguesa"
+  ],
+  "Águilas Doradas": [
+    "Aguilas",
+    "Aguilas Rionegro"
+  ],
+  "Red Bull Bragantino": [
+    "RB Bragantino",
+    "Bragantino"
+  ],
+  "Club Sportivo Trinidense": [
+    "Sportivo Trinidense","Trinidense"
+  ],
+  "CD Godoy Cruz Antonio Tomba": [
+    "Godoy Cruz"
+  ],
+  "FC Magpies": [
+    "Magpies Gibraltar",
+    "Magpies"
+  ],
+  "FK Sumqayit": [
+    "Sumqayit"
+  ],
+  "Tampereen Ilves": [
+    "Ilves Tampere"
+  ],
+  "Shelbourne FC": [
+    "Shelbourne Dublin",
+    "Shelbourne"
+  ],
+  "Puskás Akadémia FC": [
+    "Puskas Akademia",
+    "Akademia"
+  ],
+  "FK Tobol Kostanay": [
+    "Tobol Kostanay",
+    "Tobol"
+  ],
+  "SK Brann": [
+    "Brann"
+  ],
+  "FK Mornar Bar": [
+    "Mornar Bar"
+  ],
+  "Ararat-Armenia FC": [
+    "Ararat-Armenia",
+    "Ararat Armenia",
+    "Ararat"
+  ],
+  "FK Torpedo-BelAZ Zhodino": [
+    "Torpedo Zhodino"
+  ],
+  "KF Partizani Tirana": [
+    "Partizani Tirana",
+    "Partizani"
+  ],
+  "KF Drita": [
+    "Drita Gjilan",
+    "Drita"
+  ],
+  "Újpest FC": [
+    "Újpest FC",
+    "Újpest Budapest"
+  ],
+  "Dundalk FC": [
+    "Dundalk Ireland",
+    "Dundalk"
+  ],
+  "Saint Johnstone FC": [
+    "Saint Johnstone",
+    "St Johnstone"
+  ],
+  "Hammarby IF": [
+    "Hammarby"
+  ],
+  "RSC Anderlecht": [
+    "Royal Sporting Club Anderlecht",
+    "Anderlecht"
+  ],
+  "Sivasspor Kulübü": [
+    "Sivasspor",
+    "Sivaspor"
+  ],
+  "FC Paços de Ferreira": [
+    "Pacos de Ferreira",
+    "Pacos Ferreira"
+  ],
+  "Odense Boldklub": [
+    "OB Odense",
+    "OB"
+  ],
+  "Aarhus Gymnastikforening": [
+    "Aarhus GF",
+    "AGF Aarhus",
+    "AGF"
+  ],
+  "FC Zimbru Chişinău": [
+    "Zimbru Chisinau",
+    "Zimbru"
+  ],
+  "Alashkert FC": [
+    "Alashkert Yerevan",
+    "Alashkert"
+  ],
+  "Djurgårdens IF": [
+    "Djurgardens",
+    "DIF"
+  ],
+  "Kecskeméti TE": [
+    "Kecskeméti"
+  ],
+  "KF Tirana": [
+    "Tirana Albania",
+    "Tirana"
+  ],
+  "NK Osijek": [
+    "Osijek Croatia",
+    "Osijek"
+  ],
+  "Zalaegerszegi TE": [
+    "Zalaegerszegi"
+  ],
+  "FC Progrès Niederkorn": [
+    "Progres Niederkorn",
+    "Niederkorn"
+  ],
+  "Hibernian FC": [
+    "Hibernian",
+    "Hib"
+  ],
+  "Inter Club d'Escaldes": [
+    "Inter Escaldes",
+    "Inter d'Escaldes"
+  ],
+  "Neftçi PFC Baki": [
+    "Neftci Baki",
+    "Neftci"
+  ],
+  "Adana Demir Spor Kulübü": [
+    "Adana Demirspor",
+    "Demirspor"
+  ],
+  "NK Maribor": [
+    "Maribor Slovenia",
+    "Maribor"
+  ],
+  "CA Osasuna": [
+    "Club Atletico Osasuna",
+    "Osasuna"
+  ],
+  "ATK": [
+    "Mohun Bagan SG",
+    "Mohun Bagan Super Giant",
+    "Mohun Bagan"
+  ],
+  "Delhi Dynamos FC": [
+    "Odisha FC",
+    "Odisha"
+  ],
+  "Stade Rennais FC" : [
+    "Rennes"
+  ],
+  "Helsingin Jalkapalloklubi" : [
+    "HJK"
+  ],
+  "CD Santa Clara" : [
+    "Santa Clara"
+  ],
+  "Kuopion Palloseura" : [
+    "KuPS"
+  ],
+  "Tottenham Hotspur fc": [
+    "spurs",
+    "Tottenham"
+  ],
+  "Royal Standard de Liège" : [
+      "Standard Liege"
+  ],
+  "FK Lokomotiv Moskva" : [
+    "Lokomotiv Moskva",
+    "Lokomotiv Moscow"
+  ],
+  "Club de Gimnasia y Esgrima La Plata" : [
+    "Gimnasia y Esgrima La Plata",
+    "Gimnasia La Plata",
+    "Gimnasia LP",
+    "Gimnasia"
+  ],
+  "CD Guadalajara" : [
+    "Guadalajara",
+    "Chivas",
+    "Chivas de Guadalajara"
+  ],
+  "Club Universidad Nacional" : [
+    "Pumas",
+    "Universidad Nacional",
+    "UNAM",
+    "Pumas UNAM"
+  ],
+  "Kieler SV Holstein 1900":[
+    "Holstein Kiel"
+  ],
+  "Düsseldorfer TuS Fortuna 1895":[
+    "Fortuna Düsseldorf"]
 }
